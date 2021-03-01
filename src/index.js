2d3bd1d5350bec6250d0162e04fe9abb181544b1
sin wan wildy <sw.wildy@gmail.com>
2021-02-28 16:27:34 -0800

    completed up to delete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form  = document.querySelector('form')
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const input = document.querySelector('#new-task-description')
+
+    const ul = document.querySelector('ul')
+    const li = document.createElement('li')
+    const text = document.createElement('text')
+    const button = document.createElement('button')
+  
+    text.textContent = input.value + " "
+  
+    button.textContent = "Delete"
+
+    button.addEventListener('click', function(e){
+      e.target.parentNode.remove()
+    })
+
+    li.appendChild(text)
+    li.appendChild(button)
+    ul.appendChild(li)
+
+    input.value = ""
+  })
 });
